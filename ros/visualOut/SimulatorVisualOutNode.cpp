86495f83e9c2913c815d34ddbd07e3a6f4ac1953
Jonas Otto <edwin.otto@uni-ulm.de>
2021-08-13 09:35:27 +0000

    simulator trajectory visualization node

---
+++ ros/visualOut/SimulatorVisualOutNode.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file SimulatorVisualOutNode.cpp
+ * @author jonasotto
+ * @date 8.8.2021
+ */
+
+#include "SimulatorVisualOutNode.hpp"
+
+#include <Trajectory/ros/InterpolatedTrajectory.hpp>
+
+SimulatorVisualOutNode::SimulatorVisualOutNode(const std::string &name) :
+    rclcpp::Node(name),
+    tx(SimulatorSHM::SERVER, SHM_VISOUT_ID),
+    trajectorySub(create_subscription<spatz_interfaces::msg::Trajectory>(
+            "trajectory", rclcpp::QoS(1).best_effort(),
+            [this](spatz_interfaces::msg::Trajectory::ConstSharedPtr m) { onTrajectoryIn(std::move(m)); })) {
+    if (!tx.attach()) {
+        RCLCPP_ERROR(get_logger(), "Cannot open shared memory interface!");
+        throw std::runtime_error("Cannot open shared memory interface!");
+    }
+    RCLCPP_INFO(get_logger(), "Node starting");
+}
+
+void SimulatorVisualOutNode::onTrajectoryIn(spatz_interfaces::msg::Trajectory::ConstSharedPtr trajectoryMessage) {
+
+    VisualizationOut *visOut = tx.lock(SimulatorSHM::WRITE_OVERWRITE_OLDEST);
+
+    if (nullptr == visOut) {
+        RCLCPP_ERROR(get_logger(), "Cannot send visualization output: Fifo full!");
+    } else {
+        InterpolatedTrajectory trajectory(*trajectoryMessage);
+        int N = sizeof(visOut->trajectoryPoints) / sizeof(visOut->trajectoryPoints[0]);
+        double stepSize = (trajectory.endTime() - trajectory.startTime()) / N;
+
+        for (int i = 0; i < N; i++) {
+            double t = trajectory.startTime() + stepSize * i;
+            visOut->trajectoryPoints[i] = trajectory.pos(t);
+        }
+
+        RCLCPP_INFO(get_logger(), "Sending trajectory visualization to simulator");
+        tx.unlock(visOut);
+    }
+}
---
+++ ros/visualOut/SimulatorVisualOutNode.hpp
@@ -0,0 +1,31 @@
+/**
+ * @file SimulatorVisualOutNode.hpp
+ * @author jonasotto
+ * @date 8.8.2021
+ */
+
+#ifndef SIMULATORFILTERS_SIMULATORVISUALOUTNODE_HPP
+#define SIMULATORFILTERS_SIMULATORVISUALOUTNODE_HPP
+
+#include <SimulatorFilters/lib/shm_ids.h>
+#include <SimulatorFilters/lib/shmcomm.h>
+#include <rclcpp/rclcpp.hpp>
+#include <spatz_interfaces/msg/trajectory.hpp>
+
+/**
+ * @brief Node receiving trajectory for visualization in simulator
+ * @ingroup ROSNodes
+ */
+class SimulatorVisualOutNode : public rclcpp::Node {
+  public:
+    explicit SimulatorVisualOutNode(const std::string &name);
+
+  private:
+    SimulatorSHM::SHMComm<VisualizationOut> tx;
+
+    rclcpp::Subscription<spatz_interfaces::msg::Trajectory>::SharedPtr trajectorySub;
+    void onTrajectoryIn(spatz_interfaces::msg::Trajectory::ConstSharedPtr trajectory);
+};
+
+
+#endif // SIMULATORFILTERS_SIMULATORVISUALOUTNODE_HPP
