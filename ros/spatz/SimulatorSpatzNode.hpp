d2c1c636b90fd80299f41e811ba5e901d4168644
Jonas Otto <edwin.otto@uni-ulm.de>
2021-12-08 11:56:38 +0000

    Spatz Conversion Node

--- ros/spatz/SimulatorSpatzNode.hpp
+++ ros/spatz/SimulatorSpatzNode.hpp
@@ -11,19 +11,11 @@
 #include <SimulatorFilters/lib/shmcomm.h>
 #include <Spatz/lib/Spatz.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <sensor_msgs/msg/range.hpp>
 #include <spatz_interfaces/msg/rc_mode.hpp>
 #include <spatz_interfaces/msg/spatz.hpp>
-#include <tf2_ros/static_transform_broadcaster.h>
-#include <tf2_ros/transform_broadcaster.h>
-#include <visualization_msgs/msg/marker.hpp>
 
 /**
- * @brief Node receiving Spatz from simulator
- *
- * Mainly publishes spatz and rc-mode.
- * Additionally publishes the laser measurement separately, map->spatz transformation, box-marker for rviz.
- * Optionally publishes simulation time from spatz input (use_sim_time parameter).
+ * @brief Node receiving Spatz from simulator. Publishes spatz and rc-mode.
  * @ingroup ROSNodes
  */
 class SimulatorSpatzNode : public rclcpp::Node {
@@ -45,32 +37,13 @@ class SimulatorSpatzNode : public rclcpp::Node {
 
     rclcpp::Publisher<spatz_interfaces::msg::Spatz>::SharedPtr spatzPublisher;
     rclcpp::Publisher<spatz_interfaces::msg::RCMode>::SharedPtr rcModePublisher;
-    rclcpp::Publisher<sensor_msgs::msg::Range>::SharedPtr laserPublisher;
-    rclcpp::Publisher<sensor_msgs::msg::Range>::SharedPtr rearLaserPublisher;
-
-    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr markerPublisher;
-
-    tf2_ros::TransformBroadcaster tfBroadcaster;
-    tf2_ros::StaticTransformBroadcaster tfStaticBroadcaster;
 
     rclcpp::TimerBase::SharedPtr timer;
     void timerCallback();
 
-    void onSpatzLivelinessLost(const rclcpp::QOSLivelinessLostInfo &info);
+    void onSpatzLivelinessLost(const rclcpp::QOSLivelinessLostInfo &info) const;
 
-    env::Spatz spatzFromHWIn(const HardwareIn &hwIn);
-
-    static auto buildSpatzTransform(const rclcpp::Time &stamp, const env::Spatz &spatz)
-            -> geometry_msgs::msg::TransformStamped;
-
-    static sensor_msgs::msg::Range buildLaserMessage(const rclcpp::Time &stamp, float measurement);
-
-    static visualization_msgs::msg::Marker buildSpatzMarker(const rclcpp::Time &stamp);
-
-    /**
-     * Publish position of laser relative to spatz
-     */
-    void publishStaticLaserTF();
+    static env::Spatz spatzFromHWIn(const HardwareIn &hwIn);
 };
 
 #endif // SIMULATORFILTERS_SIMULATORSPATZNODE_HPP
